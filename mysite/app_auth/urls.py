f1539fcca2e3b4eef57e2665777d5b6ffe8e03d3
KwipL <136827882+KwipL@users.noreply.github.com>
2023-08-29 18:11:35 +0300

    Add files via upload

---
+++ mysite/app_auth/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import my_login, profile, my_logout
+
+
+urlpatterns = [
+    path("login/", my_login, name="login"),
+    path("profile/", profile, name="profile")
+]
